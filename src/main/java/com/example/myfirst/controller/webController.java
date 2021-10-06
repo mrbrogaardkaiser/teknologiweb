958b8e6ce9af14c173a704f197a4575f3449bb7f
Lars <larsbrogaard86@gmail.com>
2021-10-06 09:51:31 +0200

    vwerv

---
+++ src/main/java/com/example/myfirst/controller/webController.java
@@ -0,0 +1,14 @@
+package com.example.myfirst.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class webController {
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+}
